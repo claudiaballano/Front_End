30d1264a6e1c1579f1cbe0aaa4aae09af0c7a787
CLAUDIA BALLANO <claudiaballanosanchez@gmail.com>
2021-01-27 11:08:46 +0100

    initial commit scissors game

---
+++ Frameworks_JavaScript/Vue/09_Rock_Paper-scissors_Master/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            score:0,
+        }
+    },
+    mounted() {
+    },
+    methods: {
+        
+    },
+    watch: {
+    }
+})
+
+app.mount('#app')
