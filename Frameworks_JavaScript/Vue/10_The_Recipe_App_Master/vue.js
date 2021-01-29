40e9976d49de14d8b3276ae07546cde064c32444
CLAUDIA BALLANO <claudiaballanosanchez@gmail.com>
2021-01-29 10:59:42 +0100

    borrar

---
+++ Frameworks_JavaScript/Vue/10_The_Recipe_App_Master/vue.js
@@ -0,0 +1,24 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            
+
+        }
+    },
+    mounted() {
+        
+      },
+    methods: {
+        
+    },
+    watch: {
+
+    }
+})
+
+app.mount('#app')
+
+
+
+
+//You currently have 0 recipes stored in your Recipe App! Add a  Recipe
